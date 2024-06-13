3a376a8b0c73ee3430034db063d9ed5597c4e2cb
AP2015 <arian.press2015@gmail.com>
2024-06-13 17:44:50 +0330

    fix(userModel): make fields mandatory

--- models/user.go
+++ models/user.go
@@ -4,7 +4,7 @@ import "github.com/jinzhu/gorm"
 
 type User struct {
 	gorm.Model
-	Username string `gorm:"unique;not null"`
-	Email    string `gorm:"unique;not null"`
-	Password string `gorm:"not null"`
+	Username string `gorm:"unique;not null" json:"username" binding:"required"`
+	Email    string `gorm:"unique;not null" json:"email" binding:"required"`
+	Password string `gorm:"not null" json:"password" binding:"required"`
 }
