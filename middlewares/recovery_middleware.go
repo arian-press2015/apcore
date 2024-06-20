c92a715b1bcebe62053e1460f47bd2d69a2915b9
AP2015 <arian.press2015@gmail.com>
2024-06-21 01:51:13 +0330

    feat(recoveryMiddleware): add stacktrace to panic logs

--- middlewares/recovery_middleware.go
+++ middlewares/recovery_middleware.go
@@ -1,12 +1,15 @@
 package middlewares
 
 import (
+	"apcore/logger"
 	"apcore/messages"
 	"apcore/response"
 	"log"
 	"net/http"
+	"runtime/debug"
 
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func RecoveryMiddleware() gin.HandlerFunc {
@@ -14,6 +17,12 @@ func RecoveryMiddleware() gin.HandlerFunc {
 		defer func() {
 			if err := recover(); err != nil {
 				log.Printf("Recovered from panic: %v", err)
+				stackTrace := debug.Stack()
+				logger.Error("Recovered from panic",
+					zap.Any("error", err),
+					zap.String("request", c.Request.URL.Path),
+					zap.ByteString("stack", stackTrace),
+				)
 				trackID, _ := c.Get("trackId")
 
 				if resp, exists := c.Get("response"); exists {
