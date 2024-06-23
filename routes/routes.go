a090dce0ee14e64df2157d7c8d6b04d5fe1411ab
AP2015 <arian.press2015@gmail.com>
2024-06-24 01:20:53 +0330

    feat(routes): add sample route for file upload

--- routes/routes.go
+++ routes/routes.go
@@ -2,7 +2,13 @@ package routes
 
 import (
 	"apcore/controllers"
+	"apcore/messages"
 	"apcore/middlewares"
+	"apcore/response"
+	"apcore/utils/fileupload"
+	"fmt"
+	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"go.uber.org/fx"
@@ -20,12 +26,18 @@ func NewRouter() *gin.Engine {
 type Routes struct {
 	controllers       *controllers.Controllers
 	jwtAuthMiddleware *middlewares.JWTAuthMiddleware
+	fileUploader      *fileupload.LocalFileUploader
 }
 
-func NewRoutes(controllers *controllers.Controllers, jwtAuthMiddleware *middlewares.JWTAuthMiddleware) *Routes {
+func NewRoutes(
+	controllers *controllers.Controllers,
+	jwtAuthMiddleware *middlewares.JWTAuthMiddleware,
+	fileUploader *fileupload.LocalFileUploader,
+) *Routes {
 	return &Routes{
 		controllers:       controllers,
 		jwtAuthMiddleware: jwtAuthMiddleware,
+		fileUploader:      fileUploader,
 	}
 }
 
@@ -46,4 +58,37 @@ func (r *Routes) SetupRoutes(router *gin.Engine) {
 	})
 
 	router.Static("/public", "./public")
+
+	router.POST("/fileupload", r.fileUploadHandler)
+}
+
+func (r *Routes) fileUploadHandler(c *gin.Context) {
+	file, err := c.FormFile("file")
+	if err != nil {
+		response.Error(c, nil, fmt.Sprintf("get form err: %s", err.Error()), http.StatusBadRequest)
+		return
+	}
+
+	fileContent, err := file.Open()
+	if err != nil {
+		response.Error(c, nil, fmt.Sprintf("open file err: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+	defer fileContent.Close()
+
+	uploadDir := "./public/images"
+
+	err = os.MkdirAll(uploadDir, os.ModePerm)
+	if err != nil {
+		response.Error(c, nil, fmt.Sprintf("could not create upload directory: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	filename, err := r.fileUploader.Save(fileContent, uploadDir, file.Filename)
+	if err != nil {
+		response.Error(c, nil, fmt.Sprintf("upload file err: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	response.Success(c, gin.H{"filename": filename}, messages.MsgSuccessful, nil, http.StatusOK)
 }
