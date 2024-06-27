591b93d63e54d2802f584e53c1435ae9e5a4b6f2
AP2015 <arian.press2015@gmail.com>
2024-06-27 16:22:31 +0330

    chore: fix spacings

--- models/admin.go
+++ models/admin.go
@@ -2,9 +2,9 @@ package models
 
 type Admin struct {
 	BaseModel
-	Username         string `gorm:"unique;not null" json:"username" binding:"required"`
-	Phone            string `gorm:"unique;not null;size:11" json:"phone" binding:"required"`
-	ProfileImage     string `json:"profile_image"`
-	Password         string `gorm:"not null" json:"password" binding:"required"`
-	TOTPSecret       string `json:"totp_secret"`
+	Username     string `gorm:"unique;not null" json:"username" binding:"required"`
+	Phone        string `gorm:"unique;not null;size:11" json:"phone" binding:"required"`
+	ProfileImage string `json:"profile_image"`
+	Password     string `gorm:"not null" json:"password" binding:"required"`
+	TOTPSecret   string `json:"totp_secret"`
 }
