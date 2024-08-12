07dfcd624ec333b13feeb52a90f468fddb98e8ee
AP2015 <arian.press2015@gmail.com>
2024-08-12 21:30:29 +0330

    feat(models): add slug to categories and template_category

---
+++ migrations/000014_add_slug_to_category_and_template.down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE categories
+DROP COLUMN slug;
+
+ALTER TABLE category_templates
+DROP COLUMN slug;
--- models/category_template.go
+++ models/category_template.go
@@ -3,6 +3,7 @@ package models
 type CategoryTemplate struct {
 	BaseModel
 	Name        string            `gorm:"not null" json:"name"`
+	Slug        string            `gorm:"unique;not null" json:"slug"`
 	Description string            `gorm:"not null" json:"description"`
 	Products    []TemplateProduct `gorm:"foreignKey:TemplateID" json:"products"`
 }
